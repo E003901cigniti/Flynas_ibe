29df4cfb9d47b075d311204030f9e9960d27739e
E003901cigniti <RahuRam.k_33723617+E003901cigniti@users.noreply.github.com>
2018-07-18 22:12:52 +0530

    Corrected dir path in isFileDownloaded() method

--- Flynas/src/test/java/flynas/web/workflows/BookingPageFlow.java
+++ Flynas/src/test/java/flynas/web/workflows/BookingPageFlow.java
@@ -4238,18 +4238,18 @@ public class BookingPageFlow<RenderedWebElement> extends BookingPageLocators{
 		}
 		
 		public void isFileDownloaded(String fileName) throws Throwable {
-			  File dir = new File(System.getenv("USERPROFILE")+"\\Documents");
-			  File[] dirContents = dir.listFiles();
-			  flag = false;			
-			  for (int i = 0; i < dirContents.length; i++) {
-			      if (dirContents[i].getName().equals(fileName)) {
+			File dir = new File(System.getenv("USERPROFILE")+"\\Downloads");
+			File[] dirContents = dir.listFiles();
+			flag = false;		
+			for (int i = 0; i < dirContents.length; i++) {
+			    if (dirContents[i].getName().equals(fileName)) {
 			          // File has been found, it can now be deleted:
 			          dirContents[i].delete();
 			          flag = true;
 			          Reporter.SuccessReport("Verifying file download", "File successfully Downloaded");
 			          break;
-			          }
-			      }
+			        }
+			    }
 			   if(flag== false){
 				  Reporter.failureReport("Verifying file download", "File not found in download path");
 			  }			     
